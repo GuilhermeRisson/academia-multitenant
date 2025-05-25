12b9e2d6175b54c2fe6f402ef31ca4bf14d83bd3
GuilhermeRisson <guilhermerisson99@gmail.com>
2025-05-25 18:48:05 -0300

    chore: remove unnecessary files and clean up the project

--- app/Database/DatabaseManager.php
+++ app/Database/DatabaseManager.php
@@ -10,18 +10,14 @@ class DatabaseManager extends BaseDatabaseManager
 {
     public function connection($name = null)
     {
-        // Se não houver tenant definido, usa a conexão padrão
         if (!tenancy()->initialized) {
             return parent::connection($name);
         }
 
-        // Obtém o tenant atual
         $tenant = tenant();
 
-        // Nome da conexão do tenant (usando o ID ou domínio)
         $connectionName = 'tenant_' . $tenant->id;
 
-        // Configura a conexão se não existir
         if (!isset($this->app['config']['database.connections.' . $connectionName])) {
             $this->makeTenantConnection($tenant, $connectionName);
         }
@@ -31,13 +27,10 @@ class DatabaseManager extends BaseDatabaseManager
 
     protected function makeTenantConnection($tenant, $connectionName)
     {
-        // Configuração baseada no seu banco de dados principal
         $config = config('database.connections.mysql');
         
-        // Modifica o nome do banco de dados para o do tenant
-        $config['database'] = 'academia_' . $tenant->id; // ou outro padrão que preferir
-        
-        // Adiciona a nova conexão à configuração
+        $config['database'] = 'academia_' . $tenant->id; 
+    
         config(['database.connections.' . $connectionName => $config]);
     }
 }
